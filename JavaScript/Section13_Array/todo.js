e5c1bfcddc5b190b4a5de4f269cdfd90b7c600e4
teraochart <35474618+teraochart@users.noreply.github.com>
2019-07-02 13:40:07 -0700

    List and Delete
    Update list feature to show index and print on items own line
    Added delete feature

--- JavaScript/Section13_Array/todo.js
+++ JavaScript/Section13_Array/todo.js
@@ -1,25 +1,50 @@
 //Asks you what you want to do
 var input = prompt("What would you like to do?")
-var todos = ["Pet Korra",];
-
+var todos = ["Pet Korra", "Eat Pizza", "Play MH"];
 
 while (input !== "quit") {
-    //If new add to array
+    //Add item
   if(input ==="list") {
-    console.log(todos);
+    listTodos();
   }
 
-//If list console.log current array
+//List items
 
   else if (input === "new" ) {
-    var item = prompt("What would you like to add?")
-    todos.push(item)
+    addTodo();
   }
+
+//Delete a specific item
+  else if (input === "delete") {
+    removeTodo();
+
+  }
+
 //Ask for new input again
   input = prompt("What would you like to do?")
 
 }
-//If quit end app
+//End the app
 console.log("App closed!")
 
 
+function listTodos(){
+  console.log("-----------------------")
+  todos.forEach(function(todo, i){
+    console.log(i + ": " + todo);
+  });
+  console.log("-----------------------")
+}
+
+function addTodo(){
+  var item = prompt("What would you like to add?")
+  todos.push(item)
+  console.log("Added " + item + "to todo list!")
+}
+
+function removeTodo(){
+  var del = prompt("What is the index of the item you want to delete?");
+
+  todos.splice(del, 1);
+  console.log("Todo removed!")
+}
