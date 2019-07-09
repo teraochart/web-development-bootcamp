caf5c80270a2542904e33d1494680f3804430088
teraochart <35474618+teraochart@users.noreply.github.com>
2019-07-08 17:24:37 -0700

    Select and manipulate exercises and notes

---
+++ JavaScript/Section15_DOM/selectorsIntro.js
@@ -0,0 +1,22 @@
+var tag = document.getElementById("highlight");
+
+var stuff = document.getElementsByClassName("bolded");
+
+var tags = document.getElementsByTagName("body");
+
+
+
+var qtag = document.querySelector("#highlight");
+
+var qstuff = document.querySelectorAll(".bolded");
+
+
+document.querySelector("#first");
+
+document.querySelector(".special");
+
+document.getElementById("first")
+
+var p1 = document.querySelector("p.special#first")
+
+
