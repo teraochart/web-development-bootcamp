d0c1c32573a43deeedb7fe99720949288b3f2f86
teraochart <35474618+teraochart@users.noreply.github.com>
2019-09-04 11:28:52 -0700

    Features: Easy and Hard Mode

--- color-game/colorGame.js
+++ color-game/colorGame.js
@@ -1,14 +1,55 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var rgbDisplay = document.querySelector("#rgbDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn")
+var hardBtn = document.querySelector("#hardBtn")
+
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    rgbDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
+    for (var i = 0; i < squares.length; i++) {
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.display= "none";
+        }
+    }
+    
+
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    rgbDisplay.textContent = pickedColor;
+    resetButton.textContent = "New Colors";
+    messageDisplay.textContent = "";
+    h1.style.backgroundColor = "#232323";
+    for (var i = 0; i < squares.length; i++) {
+    //add colors to squares
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display= "block";
+    }
+});
+
 
 resetButton.addEventListener("click", function(){
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     rgbDisplay.textContent = pickedColor;
     resetButton.textContent = "New Colors";
